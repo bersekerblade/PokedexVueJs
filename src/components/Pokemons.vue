6b1cf27bbc75a607a3d46b295c934fa802b1ae31
Faiz <hairrichtalk@gmail.com>
2022-10-08 20:46:51 +0700

    Add Pagination

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="container">
-        <AsyncPokemon v-for="(pokemon, index) in temp" :key="index" :pokemon="pokemon" />
+        <AsyncPokemon v-for="(pokemon, index) in paginatedOrders" :key="index" :pokemon="pokemon" />
     </div>
+    <div style="margin: 20px;">
+        <!-- <button @click="loadMore" v-if="currentPage < temp.length">load more</button> -->
+        <button @click="loadMore" style="margin-top: 10px;">Load More</button>
+        <button @click="loadBefore">Load Before</button>
+    </div>
+
 </template>
 
 <script>
 import usePokemons from '@/modules/pokemons';
 import { defineAsyncComponent } from 'vue';
 import Loading from '@/components/Loading.vue';
+
 
 
 const AsyncPokemon = defineAsyncComponent({
@@ -18,16 +25,56 @@ const AsyncPokemon = defineAsyncComponent({
 })
 
 export default {
+    
     components: {
-        AsyncPokemon
+        AsyncPokemon,
     },
+
+    data() {
+        return{
+        currentPage: 1,
+        maxPerPage: 8,
+        showReadMore: true,
+        }
+    },
+
     async setup(){
         const { temp, error, load } = usePokemons();
 
         await load();
 
-        return { temp, error }
+        return { 
+            temp, 
+            error, 
+        }
+        
     },
+
+    
+    computed: {
+    totalResults() {
+      return Object.keys(this.temp).length;
+    },
+    pageCount() {
+      return Math.ceil(this.totalResults / this.maxPerPage);
+    },
+    pageOffest() {
+      return this.maxPerPage * this.currentPage;
+    },
+    paginatedOrders() {
+    return this.temp.slice(0, this.currentPage * this.maxPerPage);
+    }
+  },
+
+  methods: {
+    loadMore() {
+      this.currentPage +=1 ;
+    },
+
+    loadBefore() {
+      this.currentPage -=1 ;
+    }
+  }
 }
 
 </script>
